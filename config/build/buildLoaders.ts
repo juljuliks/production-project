e2f02c31d0b74705926f3d75ca11ad079fbda498
julia <juljuliks@gmail.com>
2023-02-04 22:07:20 +0300

    fsd setup

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
 import webpack from "webpack";
-import MiniCssExtractPlugin from 'mini-css-extract-plugin'
-import { BuildOptions } from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
@@ -9,20 +9,21 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         use: [
             isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
             {
-                loader: 'css-loader',
+                loader: "css-loader",
                 options: {
                     modules: {
                         auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                        localIdentName: isDev ? 
-                        '[path][name]__[local]--[hash:base64:5]' : 
-                        '[hash:base64:8]'
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]'
                     },
                 }
             },
-            'sass-loader',
-        ]
+            "sass-loader",
+        ],
     }
 
+    // Если не используем тайпскрипт - нужен babel-loader
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,4 +1,4 @@
-import webpack from 'webpack';
+import webpack from 'webpack'
 import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
 import {BuildEnv, BuildPaths} from "./config/build/types/config";
 import path from "path";
@@ -10,11 +10,13 @@ export default (env: BuildEnv) => {
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
         build: path.resolve(__dirname, 'build'),
         html: path.resolve(__dirname, 'public', 'index.html'),
+        src: path.resolve(__dirname, 'src'),
     }
 
     const mode = env.mode || 'development';
-    const isDev = mode === 'development';
     const PORT = env.port || 3000;
+
+    const isDev = mode === 'development';
 
     const config: webpack.Configuration = buildWebpackConfig({
         mode,
@@ -23,5 +25,5 @@ export default (env: BuildEnv) => {
         port: PORT,
     })
 
-    return config;
+    return config
 };
