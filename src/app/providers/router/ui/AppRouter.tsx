8750a21eba95cdee7bc7ebc2d6127ff10a82403c
Julia Glukhova <jglukhova@web3tech.ru>
2023-02-24 19:23:12 +0300

    add 404 page, loaders

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,22 +1,23 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader';
 
 function AppRouter() {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                {Object.values(routeConfig).map(({ path, element }) => (
-                    <Route
-                        key={path}
-                        path={path}
-                        element={
+        <Routes>
+            {Object.values(routeConfig).map(({ path, element }) => (
+                <Route
+                    key={path}
+                    path={path}
+                    element={(
+                        <Suspense fallback={<PageLoader />}>
                             <div className="page-wrapper">{element}</div>
-                        }
-                    />
-                ))}
-            </Routes>
-        </Suspense>
+                        </Suspense>
+                    )}
+                />
+            ))}
+        </Routes>
     );
 }
 
