1b4c13e7aa4a3ee1839ab445ac23554465580b8a
julia <juljuliks@gmail.com>
2023-02-25 14:31:56 +0300

    add error boundary

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import { BugButton } from 'app/providers/ErrorBoundary';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
-    const {t} = useTranslation()
+    const { t } = useTranslation();
 
     return (
         <div>
+            <BugButton />
             {t('Главная страница')}
         </div>
     );
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,24 +2,24 @@ import { useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { LangSwitcher } from 'widgets/LangSwitcher';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
-import cls from './Sidebar.module.scss'
+import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
-  className?: string,
+    className?: string,
 }
 
-export const Sidebar = ({className}: SidebarProps) => {
-  const [collapsed, setCollapsed] = useState(false)
+export const Sidebar = ({ className }: SidebarProps) => {
+    const [collapsed, setCollapsed] = useState(false);
 
-  const onToggle = () => setCollapsed(prev => !prev)
+    const onToggle = () => setCollapsed((prev) => !prev);
 
-  return (
-    <div className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}>
-      <button onClick={onToggle}>toggle</button>
-      <div className={cls.switchers}>
-        <ThemeSwitcher />
-        <LangSwitcher/>
-      </div>
-    </div>
-  );
+    return (
+        <div className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
+            <button onClick={onToggle}>toggle</button>
+            <div className={cls.switchers}>
+                <ThemeSwitcher />
+                <LangSwitcher />
+            </div>
+        </div>
+    );
 };
