ce3b6a0d6ab877cf391a7ff1b058da8ea8fb2276
julia <juljuliks@gmail.com>
2023-02-26 17:53:55 +0300

    fix

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,6 +1,7 @@
+import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
-const AboutPage = () => {
+const AboutPage: FC = () => {
     const { t } = useTranslation('about');
     return (
         <div>
