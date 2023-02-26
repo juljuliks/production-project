0fdcde449d8500299373922a9f37b5e91f0302cf
julia <juljuliks@gmail.com>
2023-02-26 16:45:42 +0300

    add storybook

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
-import cls from './Navbar.module.scss'
+import cls from './Navbar.module.scss';
 
 interface NavBarProps {
-  className?: string,
+    className?: string,
 }
 
-export const Navbar = ({className}: NavBarProps) => {
-  return (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-      <div className={classNames(cls.links)}>
-        <AppLink theme={AppLinkTheme.SECONDARY} to={'/'}>Главная</AppLink>
-        <AppLink theme={AppLinkTheme.PRIMARY} to={'/about'}>О сайте</AppLink>
-      </div>
-    </div>
-  );
+export const Navbar = ({ className }: NavBarProps) => {
+    const { t } = useTranslation();
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <div className={classNames(cls.links)}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/">
+                    {t('Главная')}
+                </AppLink>
+                <AppLink theme={AppLinkTheme.PRIMARY} to="/about">{t('О сайте')}</AppLink>
+            </div>
+        </div>
+    );
 };
