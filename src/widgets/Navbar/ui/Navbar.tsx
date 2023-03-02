dc685d04973f2abcce56f1abbbc9294589d61f07
julia <juljuliks@gmail.com>
2023-03-02 22:47:11 +0300

    add modal

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Modal } from 'shared/ui/Modal/Modal';
 import cls from './Navbar.module.scss';
 
 interface NavBarProps {
-    className?: string,
+  className?: string,
 }
 
 export const Navbar = ({ className }: NavBarProps) => {
-    const { t } = useTranslation();
-    return (
-        <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={classNames(cls.links)} />
-        </div>
-    );
+  const { t } = useTranslation();
+  const [isAuthModalOpen, setIsAuthModalOpen] = useState(false);
+
+  const onToggleModal = useCallback(() => {
+    setIsAuthModalOpen((prev) => !prev);
+  }, []);
+
+  return (
+    <div className={classNames(cls.Navbar, {}, [className])}>
+      <Button
+        theme={ButtonTheme.CLEAR_INVERTED}
+        className={classNames(cls.links)}
+        onClick={onToggleModal}
+      >
+        {t('Войти')}
+      </Button>
+      <Modal isOpen={isAuthModalOpen} onClose={onToggleModal}>
+        <div />
+      </Modal>
+    </div>
+  );
 };
