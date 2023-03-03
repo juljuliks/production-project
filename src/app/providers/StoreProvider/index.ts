bf2aca792cc7a57b3856b9a0ed6563e6327b96ad
julia <juljuliks@gmail.com>
2023-03-03 20:13:10 +0300

    add rtk

---
+++ src/app/providers/StoreProvider/index.ts
@@ -0,0 +1,9 @@
+import { createReduxStore } from './config/store';
+import { StateSchema } from './config/StateSchema';
+import { StoreProvider } from './ui/StoreProvider';
+
+export {
+  StoreProvider,
+  createReduxStore,
+  StateSchema,
+};
