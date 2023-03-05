6010ee0d19a115d9e789f50e543da006a00ca924
julia <juljuliks@gmail.com>
2023-03-05 12:40:56 +0300

    added tests

---
+++ src/features/AuthByUsername/model/selectors/getLoginState/getLoginsState.test.ts
@@ -0,0 +1,22 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { initialState } from '../../slice/loginSlice';
+import { getLoginState } from './getLoginsState';
+
+describe('getLoginState', () => {
+  test('should return login form values', () => {
+    const loginForm = {
+      username: 'test',
+      password: 'test',
+    };
+    const state: DeepPartial<StateSchema> = {
+      loginForm,
+    };
+    expect(getLoginState(state as StateSchema)).toEqual(loginForm);
+  });
+
+  test('should work with empty state', () => {
+    const state: DeepPartial<StateSchema> = {};
+    expect(getLoginState(state as StateSchema)).toEqual(initialState);
+  });
+});
