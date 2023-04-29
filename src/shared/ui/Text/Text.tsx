c7942b9e9a2b84372ceaa37973f9a30b663a2682
julia <juljuliks@gmail.com>
2023-04-29 16:17:31 +0300

    better semantic

--- src/shared/ui/Text/Text.tsx
+++ src/shared/ui/Text/Text.tsx
@@ -15,9 +15,18 @@ export enum TextAlign {
 }
 
 export enum TextSize {
+  S = 'size_s',
   M = 'size_m',
   L = 'size_l',
 }
+
+type HeaderTagType = 'h1' | 'h2' | 'h3';
+
+const mapSizeToHeaderTag: Record<TextSize, HeaderTagType> = {
+  [TextSize.S]: 'h3',
+  [TextSize.M]: 'h2',
+  [TextSize.L]: 'h1',
+};
 
 interface TextProps {
   className?: string,
@@ -38,9 +47,11 @@ export const Text = memo((props: TextProps) => {
     size = TextSize.M,
   } = props;
 
+  const HeaderType = mapSizeToHeaderTag[size];
+
   return (
     <div className={classNames(cls.Text, {}, [className, cls[theme], cls[align], cls[size]])}>
-      {title && <p className={cls.title}>{title}</p>}
+      {title && <HeaderType className={cls.title}>{title}</HeaderType>}
       {text && <p className={cls.text}>{text}</p>}
     </div>
   );
