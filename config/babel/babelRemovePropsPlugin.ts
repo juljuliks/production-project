961faf6515d64ccbd7be6f583d738344146483e2
Julia Glukhova <jglukhova@web3tech.ru>
2023-04-30 19:56:57 +0300

    fix

--- config/babel/babelRemovePropsPlugin.ts
+++ config/babel/babelRemovePropsPlugin.ts
@@ -5,11 +5,9 @@ export default function babelRemovePropsPlugin(): PluginItem {
     visitor: {
       Program(path, state) {
         const forbidden = state.opts.props || [];
-
         path.traverse({
           JSXIdentifier(current) {
             const nodeName = current.node.name;
-
             if (forbidden.includes(nodeName)) {
               current.parentPath.remove();
             }
