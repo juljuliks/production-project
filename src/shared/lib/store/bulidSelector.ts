3ebdf33222770f639b17c91aa3df2d4a86490ae6
Julia Glukhova <juljuliks@gmail.com>
2023-05-07 14:34:09 +0300

    add appImage with fallbacks

--- src/shared/lib/store/bulidSelector.ts
+++ src/shared/lib/store/bulidSelector.ts
@@ -4,7 +4,13 @@ import { StateSchema } from '@/app/providers/StoreProvider';
 type Selector<T> = (state: StateSchema) => T
 type Result<T> = [() => T, Selector<T>]
 
-export function buildSelector<T>(selector: Selector<T>): Result<T> {
+export function buildSelector<T>(...selectors: Selector<T>[]): Result<T> {
+  // if (selectors.length > 1) {
+  //   const selector = createSelector(...selectors);
+  //   const useSelectorHook = () => useSelector(selector);
+  //   return [useSelectorHook, selector];
+  // }
+  const [selector] = selectors;
   const useSelectorHook = () => useSelector(selector);
 
   return [useSelectorHook, selector];
--- src/shared/ui/Avatar/Avatar.tsx
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -1,12 +1,18 @@
 import { CSSProperties, useMemo } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './Avatar.module.scss';
+import { AppImage } from '../AppImage';
+import UserIcon from '../../assets/icons/user-filled.svg';
+import { Skeleton } from '../Skeleton';
+import { Icon } from '../Icon';
 
 interface AvatarProps {
   className?: string,
   src?: string
   size?: number
   alt?: string
+  fallbackInverted?: boolean;
+
 }
 
 export const Avatar = (props: AvatarProps) => {
@@ -15,7 +21,7 @@ export const Avatar = (props: AvatarProps) => {
     src,
     size = 100,
     alt,
-
+    fallbackInverted,
   } = props;
 
   const styles = useMemo<CSSProperties>(() => ({
@@ -23,8 +29,13 @@ export const Avatar = (props: AvatarProps) => {
     width: size,
   }), [size]);
 
+  const fallback = <Skeleton width={size} height={size} border="50%" />;
+  const errorFallback = <Icon inverted={fallbackInverted} width={size} height={size} Svg={UserIcon} />;
+
   return (
-    <img
+    <AppImage
+      fallback={fallback}
+      errorFallback={errorFallback}
       src={src}
       style={styles}
       className={classNames(cls.Avatar, {}, [className])}
